b7b327aaa85d4721809870408161b6a117cf225b
yion69 <thutanaing.personal@gmail.com>
2025-04-22 21:15:02 +0630

    upload update

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,105 @@
-use std::error::Error;
-use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::{ TcpListener, TcpStream}};
+    use chrono::Utc;
+    use http::{Method, StatusCode};
+    use std::{error::Error, path::PathBuf};
+    use serde::Deserialize;
+    use tower_http::cors::{Any, CorsLayer};
+    use axum::{ routing::{get, post}, Json, Router };
+    use tokio::{ io::AsyncWriteExt, net::TcpListener};
 
-async fn decode_stream (buffer: [u8; 1024]) -> String {
-
-    unsafe  {
-        let decoded = String::from_utf8_unchecked(buffer.to_vec());
-        decoded 
+    #[derive(Deserialize)]
+    struct ImageUplaod {
+        image_bytes: Vec<u8>,
+        image_type: String
     }
 
-}
+    async fn handle_get() -> Json<serde_json::Value> {
+        Json(serde_json::json!({
+            "server_running": "We Ball"
+        }))
+    }
 
-async fn handle_client (mut stream: TcpStream) -> std::io::Result<()> {
+    async fn handle_upload (Json(payload): Json<ImageUplaod>) -> () {
     
-    let mut buffer = [0; 1024];
+        println!("image type => {} {} ", payload.image_type, payload.image_bytes.len());
+        let extension = match payload.image_type.as_str() {
+            "image/jpeg" => "jpg",
+            "image/png" => "png",
+            "image/gif" => "gif",
+            "image/webp" => "webp",
+            _ => "bin"
+        };
 
-    if let Err(e) = stream.read(&mut buffer).await {
-        eprintln!("Error HandleClient Read: {}", e)
+        let date_time = Utc::now().format("%Y-%m-%d_%H:%M:%S").to_string();
+        let mut folder_path = PathBuf::from("transferred_images");
+        
+        if !folder_path.exists() {
+            tokio::fs::create_dir_all(&folder_path).await.map_err(|e| {
+                (StatusCode::INTERNAL_SERVER_ERROR, 
+                    format!("Failed to create directory: {}", e))
+                }).unwrap();
+        }
+
+        let file_name = format!("{}.{}", date_time, extension); 
+        match extension {
+            "jpg" => {
+                if !folder_path.join("JPG").exists() {
+                    tokio::fs::create_dir_all(&folder_path.join("JPG")).await.unwrap();
+                }
+                folder_path.push("JPG")
+            },
+            "png" => {
+                if !folder_path.join("PNG").exists() {
+                    tokio::fs::create_dir_all(&folder_path.join("PNG")).await.unwrap();
+                }
+                folder_path.push("PNG")
+            },
+            "gif" => {
+                if !folder_path.join("GIF").exists() {
+                    tokio::fs::create_dir_all(&folder_path.join("GIF")).await.unwrap();
+                }
+                folder_path.push("GIF")
+            },
+            "webp" => {
+                if !folder_path.join("WEBP").exists() {
+                    tokio::fs::create_dir_all(&folder_path.join("WEBP")).await.unwrap();
+                }
+                folder_path.push("WEBP")
+            },
+            _ => folder_path.push("Undefined"),
+        }
+
+        folder_path.push(file_name);
+
+        println!("{}", folder_path.display());
+
+        let mut file = tokio::fs::File::create(&folder_path).await.map_err(|e| {
+            (StatusCode::INTERNAL_SERVER_ERROR, format!("Failed to write file: {}", e))
+        }).unwrap();
+
+        file.write(&payload.image_bytes).await.unwrap();
+
+        println!("Saved {:?} ({} bytes)", &folder_path, payload.image_bytes.len());
+
     }
 
-    if let Err(e) = &stream.write(&mut buffer).await {
-        eprintln!("Error HandleClient Write: {}", e)
-    }
+    #[tokio::main]
+    async fn main() -> Result<(), Box<dyn Error>> {
+        
+        let socket_address = "127.0.0.1:3000";
+        let listener = TcpListener::bind(&socket_address).await?;
 
-    println!("Decoded Stream => {}", decode_stream(buffer).await);
-
-    Ok(())
-}
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+        let app:Router<()> = Router::new()
+            .route("/", get(handle_get))
+            .route("/upload", post(handle_upload))
+            .layer(
+                CorsLayer::new()
+                    .allow_headers(Any)
+                    .allow_origin(Any)
+                    .allow_methods([Method::GET, Method::POST])
+            );
     
-    let socket_address = "127.0.0.1:3000";
-    let listener = TcpListener::bind(&socket_address).await?;
- 
-    println!("TCP Server Running on {}", socket_address);
+        println!("TCP Server Running on {}", socket_address);
 
-    loop {
-        let (stream,_) = listener.accept().await?;
-
-        tokio::spawn( async move {
-            if let Err(e) = handle_client(stream).await {
-                eprintln!("{}", e);
-            }
-        });
+        axum::serve(listener, app).await?;
+        Ok(())
     }
-}
